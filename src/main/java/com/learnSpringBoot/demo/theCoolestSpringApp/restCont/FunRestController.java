567b7d5bbc0ef7b9d5c091b0d6b5f29869caa0c2
JonathanPersaud <persaud.jon@gmail.com>
2019-03-03 20:43:35 -0500

    SpringBoot Application Tasks: _Add custom properties _Expose new endpoint for displaying properties

--- src/main/java/com/learnSpringBoot/demo/theCoolestSpringApp/restCont/FunRestController.java
+++ src/main/java/com/learnSpringBoot/demo/theCoolestSpringApp/restCont/FunRestController.java
@@ -1,5 +1,6 @@
 package com.learnSpringBoot.demo.theCoolestSpringApp.restCont;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,23 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
+
+    // Retrieve custom properties
+    @Value("${developer.name}")
+    private String devName;
+    @Value("${developer.expertiseLevel}")
+    private String lvlPower;
+    @Value("${developer.attidude}")
+    private String atttidude;
+    @Value("${developer.fuel}")
+    private String fuel;
+
+    @GetMapping("/customProps")
+    public String showMemyProperties(){
+        return "The developer for this project is : " + devName + "\n"
+                + "He has a power level of over : " + lvlPower + "\n"
+                + "And he is fueled by: " + fuel;
+    }
 
     @GetMapping("/tacocat")
     public String sayHi(){
